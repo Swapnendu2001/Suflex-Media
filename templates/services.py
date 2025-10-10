cb01626e6cfb5d483ba8307636ea75a5732b09e8
Swapnendu2001 <swapnendu2001.sc@gmail.com>
2025-10-11 02:06:57 +0530

    Services Page desktop mode done

--- templates/services.py
+++ templates/services.py
@@ -1,6 +1,8 @@
 from components.services.hero import hero_css, hero_html
 from components.services.why_hire import why_hire_css, why_hire_html
 from components.services.perfect_fit import perfect_fit_css, perfect_fit_html
+from components.services.faq import faq_css, faq_html
+from components.services.book_strategy_cta import book_strategy_cta_css, book_strategy_cta_html
 from components.header import header_style, header_content
 from components.footer import footer_style, footer_content
 
@@ -35,6 +37,8 @@ def services():
         {hero_css()}
         {why_hire_css()}
         {perfect_fit_css()}
+        {faq_css()}
+        {book_strategy_cta_css()}
         {footer_style()}
     </head>
     <body>
@@ -43,6 +47,8 @@ def services():
             {hero_html()}
             {why_hire_html()}
             {perfect_fit_html()}
+            {faq_html()}
+            {book_strategy_cta_html()}
         </div>
         {footer_content()}
     </body>
