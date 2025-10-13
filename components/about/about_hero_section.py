29059bb1a3a5a0f94e5b1ff6b18967914e6bc26c
avijitbhuin21 <avijitbhuin21@gmail.com>
2025-10-13 12:38:28 +0530

    about us page done

--- components/about/about_hero_section.py
+++ components/about/about_hero_section.py
@@ -115,6 +115,206 @@ def about_hero_section_style():
             top: 8vw;
             width: 6vw;
         }
+
+        /* Tablet Responsive Design (768px - 1024px) */
+        @media (max-width: 1024px) and (min-width: 768px) {
+            .about-hero-section {
+                padding: 4vh 4vw;
+                gap: 4vh;
+            }
+            
+            .about-hero-intro {
+                flex-direction: column;
+                align-items: center;
+            }
+            
+            .about-hero-text {
+                max-width: 80vw;
+                top: 0;
+                text-align: center;
+            }
+            
+            .about-hero-text h1 {
+                font-size: 5vw;
+            }
+            
+            .about-hero-text h2 {
+                font-size: 5.5vw;
+            }
+            
+            .about-hero-text p {
+                font-size: 2.5vw;
+                margin-top: 2vh;
+            }
+            
+            .about-hero-image {
+                max-width: 70vw;
+                margin-top: 3vh;
+            }
+            
+            .mission-section {
+                max-width: 80vw;
+            }
+            
+            .mission-section h3 {
+                font-size: 5vw;
+            }
+            
+            .mission-section p {
+                font-size: 2.5vw;
+            }
+            
+            .mission-section .star-left {
+                left: 5vw;
+                top: -3vh;
+                width: 6vw;
+            }
+            
+            .mission-section .star-right {
+                right: 1vw;
+                bottom: -6vh;
+                width: 4vw;
+            }
+            
+            .stats-grid {
+                gap: 4vw;
+                margin-top: 4vh;
+            }
+            
+            .stat-item .label {
+                font-size: 2.5vw;
+            }
+            
+            .stat-item .value {
+                font-size: 3.5vw;
+            }
+            
+            .stats-grid .pencil-icon {
+                left: -10vw;
+                width: 8vw;
+            }
+            
+            .stats-grid .clover-icon {
+                left: 0vw;
+                width: 5vw;
+            }
+            
+            .stats-grid .spring-icon {
+                right: -8vw;
+                top: 10vw;
+                width: 7vw;
+            }
+        }
+
+        /* Mobile Responsive Design (Under 768px) */
+        @media (max-width: 767px) {
+            .about-hero-section {
+                padding: 3vh 5vw;
+                gap: 3vh;
+                margin-top: -10vh;
+            }
+            
+            .about-hero-intro {
+                flex-direction: column;
+                align-items: center;
+            }
+            
+            .about-hero-text {
+                max-width: 100%;
+                top: 0;
+                text-align: center;
+            }
+            
+            .about-hero-text h1 {
+                font-size: 7vw;
+                margin-bottom: 1vh;
+            }
+            
+            .about-hero-text h2 {
+                font-size: 8vw;
+                line-height: 1.3;
+            }
+            
+            .about-hero-text p {
+                font-size: 4vw;
+                margin-top: 2vh;
+                line-height: 1.5;
+            }
+            
+            .about-hero-text p img {
+                width: 4vw !important;
+                margin-top: 0 !important;
+            }
+            
+            .about-hero-image {
+                max-width: 90vw;
+                margin-top: 3vh;
+            }
+            
+            .mission-section {
+                max-width: 90vw;
+                padding: 0 2vw;
+            }
+            
+            .mission-section h3 {
+                font-size: 7vw;
+                margin-bottom: 2vh;
+            }
+            
+            .mission-section p {
+                font-size: 4vw;
+                line-height: 1.6;
+            }
+            
+            .mission-section .star-left {
+                left: -2vw;
+                top: -2vh;
+                width: 8vw;
+            }
+            
+            .mission-section .star-right {
+                right: -2vw;
+                bottom: -5vh;
+                width: 6vw;
+            }
+            
+            .stats-grid {
+                grid-template-columns: 1fr;
+                gap: 3vh;
+                margin-top: 4vh;
+                padding: 0 5vw;
+            }
+            
+            .stat-item {
+                width: 100%;
+            }
+            
+            .stat-item .label {
+                font-size: 4vw;
+            }
+            
+            .stat-item .value {
+                font-size: 6vw;
+                max-width: 100%;
+            }
+            
+            .stats-grid .stat-item-bottom {
+                grid-column: 1;
+                margin-top: 0;
+            }
+            
+            .stats-grid .pencil-icon {
+                display: none;
+            }
+            
+            .stats-grid .clover-icon {
+                display: none;
+            }
+            
+            .stats-grid .spring-icon {
+                display: none;
+            }
+        }
     </style>
     """
 
