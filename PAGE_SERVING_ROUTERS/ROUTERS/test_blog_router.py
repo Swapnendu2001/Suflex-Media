4a7de853d77c608f7afa3ad94374ef76c6249ea8
avijitbhuin21 <avijitbhuin21@gmail.com>
2025-10-15 15:57:13 +0530

    admin user complete final

--- PAGE_SERVING_ROUTERS/ROUTERS/test_blog_router.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from fastapi.responses import FileResponse
-
-router = APIRouter()
-
-@router.get("/test_blog")
-async def get_test_blog():
-    return FileResponse("PAGE_SERVING_ROUTERS/PAGES/Blog_sample.html")
