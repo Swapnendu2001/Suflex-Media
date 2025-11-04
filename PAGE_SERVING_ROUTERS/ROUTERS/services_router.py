710c0a52df49b333923856989d6a6124d8672f61
avijitbhuin21 <avijitbhuin21@gmail.com>
2025-11-04 20:20:48 +0530

    changes suggested

--- PAGE_SERVING_ROUTERS/ROUTERS/services_router.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from fastapi.responses import FileResponse
-
-router = APIRouter()
-
-@router.get("/services")
-async def get_services():
-    return FileResponse("PAGE_SERVING_ROUTERS/PAGES/services.html")
