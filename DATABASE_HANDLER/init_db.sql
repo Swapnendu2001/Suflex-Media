f7efd2747a40b73c827b5661115e5914ebb954cd
Swapnendu2001 <swapnendu2001.sc@gmail.com>
2025-11-23 22:27:10 +0530

    minor fixes

--- DATABASE_HANDLER/init_db.sql
+++ DATABASE_HANDLER/init_db.sql
@@ -1,8 +1,4 @@
--- DROP TABLE IF EXISTS admin_users CASCADE;
--- DROP TABLE IF EXISTS blogs CASCADE;
--- DROP TABLE IF EXISTS case_studies CASCADE;
-
-CREATE TABLE if not EXISTS admin_users (
+CREATE TABLE IF NOT EXISTS ADMIN_USERS (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     email VARCHAR(255) NOT NULL UNIQUE,
     username VARCHAR(100) NOT NULL UNIQUE,
@@ -13,7 +9,7 @@ CREATE TABLE if not EXISTS admin_users (
 );
 
 
-CREATE TABLE if not EXISTS blogs (
+CREATE TABLE IF NOT EXISTS blogs (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     slug TEXT UNIQUE,
     blogContent JSONB NOT NULL,
@@ -28,18 +24,18 @@ CREATE TABLE if not EXISTS blogs (
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE IF NOT EXISTS INDEX idx_blogs_status ON blogs(status) WHERE isDeleted = FALSE;
-CREATE IF NOT EXISTS INDEX idx_blogs_type ON blogs(type) WHERE isDeleted = FALSE;
-CREATE IF NOT EXISTS INDEX idx_blogs_date ON blogs(date DESC) WHERE isDeleted = FALSE;
-CREATE IF NOT EXISTS INDEX idx_blogs_slug ON blogs(slug) WHERE isDeleted = FALSE;
-CREATE IF NOT EXISTS INDEX idx_blogs_isDeleted ON blogs(isDeleted);
-CREATE IF NOT EXISTS INDEX idx_blogs_keyword ON blogs USING GIN(keyword);
-CREATE IF NOT EXISTS INDEX idx_blogs_blogContent ON blogs USING GIN(blogContent);
+CREATE INDEX IF NOT EXISTS idx_blogs_status ON blogs(status) WHERE isDeleted = FALSE;
+CREATE INDEX IF NOT EXISTS idx_blogs_type ON blogs(type) WHERE isDeleted = FALSE;
+CREATE INDEX IF NOT EXISTS idx_blogs_date ON blogs(date DESC) WHERE isDeleted = FALSE;
+CREATE INDEX IF NOT EXISTS idx_blogs_slug ON blogs(slug) WHERE isDeleted = FALSE;
+CREATE INDEX IF NOT EXISTS idx_blogs_isDeleted ON blogs(isDeleted);
+CREATE INDEX IF NOT EXISTS idx_blogs_keyword ON blogs USING GIN(keyword);
+CREATE INDEX IF NOT EXISTS idx_blogs_blog ON blogs USING GIN(blogContent);
 
-CREATE TABLE if not EXISTS case_studies (
+CREATE TABLE IF NOT EXISTS case_studies (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     slug TEXT UNIQUE,
-    blogContent JSONB NOT NULL,
+    case_study JSONB NOT NULL,
     status VARCHAR(50) NOT NULL DEFAULT 'draft',
     type VARCHAR(50) NOT NULL DEFAULT 'CASE STUDY',
     date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -53,11 +49,11 @@ CREATE TABLE if not EXISTS case_studies (
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE IF NOT EXISTS INDEX idx_case_studies_status ON case_studies(status) WHERE isDeleted = FALSE;
-CREATE IF NOT EXISTS INDEX idx_case_studies_type ON case_studies(type) WHERE isDeleted = FALSE;
-CREATE IF NOT EXISTS INDEX idx_case_studies_date ON case_studies(date DESC) WHERE isDeleted = FALSE;
-CREATE IF NOT EXISTS INDEX idx_case_studies_slug ON case_studies(slug) WHERE isDeleted = FALSE;
-CREATE IF NOT EXISTS INDEX idx_case_studies_isDeleted ON case_studies(isDeleted);
-CREATE IF NOT EXISTS INDEX idx_case_studies_keyword ON case_studies USING GIN(keyword);
-CREATE IF NOT EXISTS INDEX idx_case_studies_preview ON case_studies USING GIN(preview);
-CREATE IF NOT EXISTS INDEX idx_case_studies_blogContent ON case_studies USING GIN(blogContent);
+CREATE INDEX IF NOT EXISTS idx_case_studies_status ON case_studies(status) WHERE isDeleted = FALSE;
+CREATE INDEX IF NOT EXISTS idx_case_studies_type ON case_studies(type) WHERE isDeleted = FALSE;
+CREATE INDEX IF NOT EXISTS idx_case_studies_date ON case_studies(date DESC) WHERE isDeleted = FALSE;
+CREATE INDEX IF NOT EXISTS idx_case_studies_slug ON case_studies(slug) WHERE isDeleted = FALSE;
+CREATE INDEX IF NOT EXISTS idx_case_studies_isDeleted ON case_studies(isDeleted);
+CREATE INDEX IF NOT EXISTS idx_case_studies_keyword ON case_studies USING GIN(keyword);
+CREATE INDEX IF NOT EXISTS idx_case_studies_preview ON case_studies USING GIN(preview);
+CREATE INDEX IF NOT EXISTS idx_case_studies_case_study ON case_studies USING GIN(case_study);
